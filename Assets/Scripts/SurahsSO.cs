1888884d19283276979a9ad740b2fe65f7ea99e1
Nader Elshehabi <dr.elshehabi@gmail.com>
2024-01-22 20:00:29 +1100

    changes in UI. Added JSON resources

--- Assets/Scripts/SurahsSO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Surahs", menuName = "Surahs List")]
-public class SurahsSO : ScriptableObject
-{
-    [SerializeField] List<Surah> Surahs = new List<Surah>();
-}
