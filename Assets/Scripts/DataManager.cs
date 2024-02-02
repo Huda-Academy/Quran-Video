43576d88038ba331ea8861b4de72949ee02242b3
Nader Elshehabi <dr.elshehabi@gmail.com>
2024-02-03 08:43:48 +1100

    fixes JSON loading

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -7,7 +7,6 @@ using UnityEngine.UI;
 
 public class DataManager : MonoBehaviour
 {
-    [SerializeField] private Dropdown surahDropdown;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,43 +16,25 @@ public class DataManager : MonoBehaviour
     private void LoadData()
     {
         // Load JSON data from resources
-        TextAsset surahsJson = Resources.Load<TextAsset>("Data/surahs");
-        TextAsset qarisJson = Resources.Load<TextAsset>("Data/qaris");
+        TextAsset surahJson = Resources.Load<TextAsset>("Data/surahs");
+        TextAsset qariJson = Resources.Load<TextAsset>("Data/qaris");
 
-        Surah[] surahs = JsonHelper.FromJson<Surah>(surahsJson.text);
+        Surah[] surahs = JsonHelper.FromJson<Surah>(surahJson.text);
+        Qari[] qaris = JsonHelper.FromJson<Qari>(qariJson.text);
 
         foreach (Surah surah in surahs)
         {
-            Debug.Log(surah.Name);
+            Debug.Log(surah.number);
+            Debug.Log(surah.name);
+            Debug.Log(surah.juzz);
+            Debug.Log(surah.origin);
+            Debug.Log(surah.ayah);
         }
-    }
-}
 
-public static class JsonHelper
-{
-    public static T[] FromJson<T>(string json)
-    {
-        Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
-        return wrapper.Items;
-    }
-
-    public static string ToJson<T>(T[] array)
-    {
-        Wrapper<T> wrapper = new Wrapper<T>();
-        wrapper.Items = array;
-        return JsonUtility.ToJson(wrapper);
-    }
-
-    public static string ToJson<T>(T[] array, bool prettyPrint)
-    {
-        Wrapper<T> wrapper = new Wrapper<T>();
-        wrapper.Items = array;
-        return JsonUtility.ToJson(wrapper, prettyPrint);
-    }
-
-    [Serializable]
-    private class Wrapper<T>
-    {
-        public T[] Items;
+        foreach (Qari qari in qaris)
+        {
+            Debug.Log(qari.id);
+            Debug.Log(qari.name);
+        }
     }
 }
--- Assets/Scripts/DataTypes.cs
+++ Assets/Scripts/DataTypes.cs
@@ -2,34 +2,19 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
+[Serializable]
 public class Surah
 {
-    private int number;
-    private string name;
-    private int juzz;
-    private int origin;
-    private int ayah;
-
-    public int Number { get => number; set => number = value; }
-    public string Name { get => name; set => name = value; }
-    public int Juzz { get => juzz; set => juzz = value; }
-    public int Origin { get => origin; set => origin = value; }
-    public int Ayah { get => ayah; set => ayah = value; }
-
-    public Surah(int number, string name, int juzz, int origin, int ayah)
-    {
-        this.number = number;
-        this.name = name;
-        this.juzz = juzz;
-        this.origin = origin;
-        this.ayah = ayah;
-    }
-
+    public int number;
+    public string name;
+    public int juzz;
+    public int origin;
+    public int ayah;
 }
 
-public enum SurahOrigin
+[Serializable]
+public class Qari
 {
-    Meccan = 1,
-    Medinan = 2
+    public int id;
+    public string name;
 }
