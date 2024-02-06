76fbcfca48abc8a9d08095729d205964ca40c98e
Nader Elshehabi <dr.elshehabi@gmail.com>
2024-02-06 12:23:06 +1100

    loading Surah Title

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -5,16 +5,23 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using SimpleFileBrowser;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using Unity.VectorGraphics;
 
 
 public class DataManager : MonoBehaviour
 {
     // SerializeField for the DropDown
+    [Header("UI Elements")]
     [SerializeField] private TMP_Dropdown surahDropdown;
     [SerializeField] private TMP_Dropdown qariDropdown;
     [SerializeField] private TMP_InputField surahsPath;
 
+    [Header("Placeholders")]
+    [SerializeField] private GameObject surahTitle;
+
+    private GameObject currentSurahTitle = null;
 
     // Start is called before the first frame update
     void Start()
@@ -41,6 +48,8 @@ public class DataManager : MonoBehaviour
         {
             qariDropdown.options.Add(new TMP_Dropdown.OptionData(qari.name));
         }
+
+        LoadSurahTitle();
     }
 
     public void LoadSurahs()
@@ -66,6 +75,80 @@ public class DataManager : MonoBehaviour
             string fileName = System.IO.Path.GetFileName(file);
             Debug.Log(fileName);
         }
+    }
+
+    public void LoadNextSurah()
+    {
+        int currentSurah = surahDropdown.value;
+        int nextSurah = currentSurah + 1;
+
+        if (nextSurah >= surahDropdown.options.Count)
+        {
+            return;
+        }
+
+        surahDropdown.value = nextSurah;
+        LoadSurahTitle();
+    }
+
+    public void LoadPreviousSurah()
+    {
+        int currentSurah = surahDropdown.value;
+        int previousSurah = currentSurah - 1;
+
+        if (previousSurah < 0)
+        {
+            return;
+        }
+
+        surahDropdown.value = previousSurah;
+        LoadSurahTitle();
+    }
+
+    public void LoadSurahTitle()
+    {
+        int surahIndex = surahDropdown.value;
+        // Instantiate Prefab from "SVG\Surahs" folder
+        // Load the SVG file based on the surahIndex
+
+        Addressables.LoadAssetAsync<GameObject>($"Assets/SVG/Surahs/{surahIndex + 1}.svg").Completed += LoadingSurahTitle_Completed;
+
+    }
+
+    private void LoadingSurahTitle_Completed(AsyncOperationHandle<GameObject> obj)
+    {
+        if (obj.Status != AsyncOperationStatus.Succeeded)
+        {
+            Debug.LogError("Failed to load Surah Title SVG");
+            return;
+        }
+
+        if (currentSurahTitle != null)
+        {
+            Destroy(currentSurahTitle);
+        }
+
+        GameObject surahSVG = obj.Result;
+        // Set the surahSVG transform to be
+        // Anchor top right
+        // Pos X = -225
+        // Pos Y = -55
+        // Width = 350
+        // Height = 80
+        RectTransform surahRectTransform = surahSVG.GetComponent<RectTransform>();
+        surahRectTransform.anchorMin = new Vector2(1, 1);
+        surahRectTransform.anchorMax = new Vector2(1, 1);
+        surahRectTransform.pivot = new Vector2(0.5f, 0.5f);
+        surahRectTransform.anchoredPosition = new Vector2(-225, -55);
+        surahRectTransform.sizeDelta = new Vector2(350, 80);
+        surahRectTransform.localScale = Vector3.one;
+
+        // Change SVG Image color to black
+        SVGImage surahImage = surahSVG.GetComponent<SVGImage>();
+        surahImage.color = Color.black;
+
+
+        currentSurahTitle = Instantiate(surahSVG, surahTitle.transform, false);
     }
 
     // TODO
