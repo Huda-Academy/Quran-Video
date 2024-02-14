e0c236a894d18c44c7f69116744fbae4dc8fa496
Nader Elshehabi <dr.elshehabi@gmail.com>
2024-02-14 20:18:16 +1100

    reverted to Async await. working audio playing

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,20 +1,13 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.Networking;
 using System.Threading.Tasks;
-using Unity.Jobs;
-using Unity.Collections;
-using Unity.Burst;
-
+using System.Linq;
 
 [RequireComponent(typeof(AudioSource))]
 public class AudioManager : MonoBehaviour
 {
     AudioSource audioSource;
-    AudioClip audioClip;
 
     [SerializeField]
     TMP_Dropdown surahDropdown;
@@ -32,22 +25,22 @@ public class AudioManager : MonoBehaviour
         audioSource = GetComponent<AudioSource>();
     }
 
-    public void PlayAudio()
+    public async void PlayAudio()
     {
         int surahIndex = surahDropdown.value + 1;
         string surahIndexPadded = surahIndex.ToString().PadLeft(3, '0');
 
         // Search for files that start with the surah index in the path
-        string[] files = System.IO.Directory.GetFiles(QuranPath.text, surahIndexPadded + "*");
+        string audioFile = System.IO.Directory.GetFiles(QuranPath.text, surahIndexPadded + "*").FirstOrDefault();
 
-        RunJob(files[0]);
+        AudioClip audioClip = await GetAudioClip(audioFile);
 
         if (audioSource.isPlaying)
         {
             audioSource.Stop();
         }
 
-        if (audioSource != null)
+        if (audioClip != null)
         {
             audioSource.clip = audioClip;
             audioSource.Play();
@@ -58,6 +51,35 @@ public class AudioManager : MonoBehaviour
             Debug.LogError("Audio Clip is null");
         }
     }
+
+    private async Task<AudioClip> GetAudioClip(string filePath)
+    {
+        try
+        {
+            using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip("file://" + filePath, AudioType.MPEG))
+            {
+                // Send the request and wait for a response
+                await www.SendWebRequest();
+
+                if (www.result == UnityWebRequest.Result.ConnectionError)
+                {
+                    Debug.LogError(www.error);
+                    return null;
+                }
+                else
+                {
+                    AudioClip clip = DownloadHandlerAudioClip.GetContent(www);
+                    return clip;
+                }
+            }
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError(e);
+            return null;
+        }
+    }
+
     public void PauseAudio()
     {
 
@@ -71,57 +93,5 @@ public class AudioManager : MonoBehaviour
             audioSource.UnPause();
             pauseButton.text = "Pause";
         }
-
-    }
-
-    // A method to create and run the job
-    public async void RunJob(string filePath)
-    {
-        NativeReference<NativeArray<float>> audioBuffer = new NativeReference<NativeArray<float>>(Allocator.Persistent);
-        // Create a NativeArray of the MP3Job struct
-        // Initialize the struct with the file path and the audio type
-        GetQuranAudioJob getAudioJob = new GetQuranAudioJob
-        {
-            filePath = new NativeArray<char>(filePath.ToCharArray(), Allocator.Persistent),
-            audioData = audioBuffer
-        };
-
-        // Create a job handle by scheduling the job
-        JobHandle jobHandle = getAudioJob.Schedule();
-
-        // Complete the job
-        jobHandle.Complete();
-        Debug.Log("Is Complete? " + jobHandle.IsCompleted);
-
-        Debug.Log("Native Array: " + audioBuffer.Value.Length);
-
-        audioClip = AudioClip.Create("Quran Audio", audioBuffer.Value.Length, 1, 44100, false);
-        audioClip.SetData(audioBuffer.Value.ToArray(), 0);
-
-        // dispose of the NativeArray
-        getAudioJob.filePath.Dispose();
-        audioBuffer.Value.Dispose();
-        audioBuffer.Dispose();
-    }
-}
-
-[BurstCompile]
-public struct GetQuranAudioJob : IJob
-{
-    public NativeArray<char> filePath;
-    public NativeReference<NativeArray<float>> audioData;
-    public void Execute()
-    {
-        // Get the audio file
-        byte[] audioBytes = System.IO.File.ReadAllBytesAsync(new string(filePath.ToArray())).GetAwaiter().GetResult();
-        float[] floatArr = new float[audioBytes.Length / 4];
-        for (int i = 0; i < floatArr.Length; i++)
-        {
-            if (BitConverter.IsLittleEndian)
-                Array.Reverse(audioBytes, i * 4, 4);
-            floatArr[i] = BitConverter.ToSingle(audioBytes, i * 4);
-        }
-        audioData.Value = new NativeArray<float>(floatArr, Allocator.Temp);
-        Debug.Log("Inside Job: " + audioData.Value.Length);
     }
 }
