45e8790b57617e2f8499dece121bce431e5a25a5
Nader Elshehabi <dr.elshehabi@gmail.com>
2024-02-18 15:54:18 +1100

    Working audio visualisation

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -3,6 +3,8 @@ using TMPro;
 using UnityEngine.Networking;
 using System.Threading.Tasks;
 using System.Linq;
+using System.Runtime.ExceptionServices;
+using System;
 
 [RequireComponent(typeof(AudioSource))]
 public class AudioManager : MonoBehaviour
@@ -10,6 +12,17 @@ public class AudioManager : MonoBehaviour
     AudioSource audioSource;
 
     TimerManager timerManager;
+    Task timerTask;
+
+    [SerializeField]
+    GameObject audioBarsContainer;
+    [SerializeField]
+    GameObject audioBar;
+
+    GameObject[] audioBars = new GameObject[8];
+
+    float[] _samples = new float[512];
+    float[] _freqBands = new float[8];
 
     [SerializeField]
     TMP_Dropdown surahDropdown;
@@ -21,11 +34,25 @@ public class AudioManager : MonoBehaviour
     [SerializeField]
     TextMeshProUGUI pauseButton;
 
-
     private void Awake()
     {
         audioSource = GetComponent<AudioSource>();
         timerManager = GetComponent<TimerManager>();
+    }
+
+    private void Start()
+    {
+        DisplayAudioBars();
+    }
+
+    private void Update()
+    {
+        if (audioSource.isPlaying)
+        {
+            GetSpectrumAudioSource();
+            MakeFrequencyBands();
+            UpdateAudioBars();
+        }
     }
 
     public async void PlayAudio()
@@ -47,7 +74,7 @@ public class AudioManager : MonoBehaviour
         {
             audioSource.clip = audioClip;
             audioSource.Play();
-            _ = timerManager.StartTimer(audioClip.length);
+            timerTask = timerManager.StartTimer(audioClip.length);
             pauseButton.text = "Pause";
         }
         else
@@ -98,6 +125,62 @@ public class AudioManager : MonoBehaviour
             audioSource.UnPause();
             timerManager.PauseTimer(false);
             pauseButton.text = "Pause";
+        }
+    }
+
+    private void GetSpectrumAudioSource()
+    {
+        audioSource.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
+    }
+
+    private void MakeFrequencyBands()
+    {
+        int count = 0;
+        for (int i = 0; i < 8; i++)
+        {
+            float average = 0;
+            int sampleCount = (int)Mathf.Pow(2, i) * 2;
+
+            if (i == 8)
+            {
+                sampleCount += 2;
+            }
+
+            for (int j = 0; j < sampleCount; j++)
+            {
+                average += _samples[count] * (count + 1);
+                count++;
+            }
+
+            average /= count;
+
+            _freqBands[i] = average;
+        }
+    }
+
+    private void DisplayAudioBars()
+    {
+        // initial AudioBar Tranform
+        // x=-0.46, y=-0.5, z=-0.1
+        // Scale = x=0.05, y=frequency/1, z=1
+        // Add margin of 0.07
+
+        for (int i = 0; i < 8; i++)
+        {
+            if (audioBars[i] == null)
+            {
+                audioBars[i] = Instantiate(audioBar, audioBarsContainer.transform);
+                audioBars[i].transform.localPosition = new Vector3(-0.46f + (0.15f * i), -0.5f, -0.1f);
+            }
+            audioBars[i].transform.localScale = new Vector3(0.12f, 0.1f, 1);
+        }
+    }
+
+    private void UpdateAudioBars()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            audioBars[i].transform.localScale = new Vector3(0.12f, Mathf.Clamp(Mathf.Lerp(0.1f, 2.5f, _freqBands[i]), 0.1f, 1f), 1);
         }
     }
 }
